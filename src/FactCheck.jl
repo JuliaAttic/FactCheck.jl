69edce9891e3eaef0c97aa58129932d529ce639c
Zachary Allaun <zach.allaun@gmail.com>
2013-06-20 00:58:45 -0400

    The death of `nearequal`

--- src/FactCheck.jl
+++ src/FactCheck.jl
@@ -358,10 +358,6 @@ exactly(x) = (y) -> is(x, y)
 #     @fact 4.99999 => roughly(5)
 #
 
-# isapprox comes from nearequal.jl, which is currently available in
-# JuliaLang/extras. Likely to move soon...
-include(joinpath("/", split(JULIA_HOME, "/")[1:end-2]..., "extras", "nearequal.jl"))
-
 roughly(n::Number, rtol::Number, atol::Number) = (i) -> isapprox(i,n,rtol,atol)
 roughly(n::Number, tol::Number) = (i) -> isapprox(i, n, tol, tol)
 roughly(n::Number) = (i) -> isapprox(i,n)

37e1f35418b2dbc99f8412478b205a03c98ad7d2
Jake Bolewski <jakebolewski@gmail.com>
2015-07-28 21:16:42 -0400

    update example file to remove deprecated syntax

--- example/examples.jl
+++ example/examples.jl
@@ -4,22 +4,22 @@ inc(x) = x + 1
 
 facts("Succeeding examples") do
 
-    @fact 1 => 1
+    @fact 1 --> 1
 
     context("You can define contexts") do
-        @fact 1 => 1
+        @fact 1 --> 1
     end
 
     @fact_throws error("neat")
 
     context("group facts") do
-        @fact 1 => not(2)
-        @fact 2 => not(isodd)
+        @fact 1 --> not(2)
+        @fact 2 --> not(isodd)
     end
 
     context("generating facts") do
         for i=1:5
-            @fact i => i
+            @fact i --> i
         end
     end
 
@@ -27,45 +27,45 @@ end
 
 facts("Failing examples") do
 
-    @fact 1 => 2
+    @fact 1 --> 2
 
     context("strings are strings") do
-        @fact "bar" => "barr"
-        @fact "baz" => "bazz"
+        @fact "bar" --> "barr"
+        @fact "baz" --> "bazz"
     end
 
     context("some numbers are even") do
-        @fact 3 => iseven
+        @fact 3 --> iseven
     end
 
     context() do
         x = 10
-        @fact inc(inc(inc(0))) => 2
+        @fact inc(inc(inc(0))) --> 2
     end
 
     context() do
         x = 5
         y = 10
-        @fact x => y
+        @fact x --> y
     end
 
     context("throws an error") do
-        @fact error("foo") => 1
+        @fact error("foo") --> 1
     end
 
 end
 
 facts("Assertion helpers") do
 
-    @fact 1 => not(iseven)
-    @fact 1 => truthy
-    @fact nothing => falsey
-    @fact false => anything
-    @fact iseven => exactly(iseven)
-    @fact 2.499999 => roughly(2.5)
-    @fact 1 => less_than(2)
-    @fact 1 => less_than_or_equal(1)
-    @fact 2 => greater_than(1)
-    @fact 2 => greater_than_or_equal(2)
+    @fact 1 --> not(iseven)
+    @fact 1 --> truthy
+    @fact nothing --> falsey
+    @fact false --> anything
+    @fact iseven --> exactly(iseven)
+    @fact 2.499999 --> roughly(2.5)
+    @fact 1 --> less_than(2)
+    @fact 1 --> less_than_or_equal(1)
+    @fact 2 --> greater_than(1)
+    @fact 2 --> greater_than_or_equal(2)
 
 end
